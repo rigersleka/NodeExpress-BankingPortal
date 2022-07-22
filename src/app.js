86c5468bf2b7153f3527ce227e8b69d9cf5b3795
Rigers Leka <rigers.leka@sisal.al>
2022-07-22 14:33:23 +0200

    fix some issues

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 
+// Initialize framework Express and add reference of the framework into a property
 const express = require('express');
-const { path } = require('path');
 const app = new express();
 
+const path = require('path');
 app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
-});
+// Index Route
+app.get('/', (req, res) => res.render('index', { title: 'Index' }));
 
-app.listen(8080);
-console.log('PS Project Running on port 3000!');
+// Listen to PORT = 3000
+app.listen(3000, () => console.log('PS Project Running on port 3000'))
