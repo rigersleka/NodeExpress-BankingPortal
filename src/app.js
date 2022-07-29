b3d71cae11ae009f16852a5c468f744077c8cfcb
Rigers Leka <rigers.leka@sisal.al>
2022-07-29 11:54:31 +0200

    updates to module 3

--- src/app.js
+++ src/app.js
@@ -5,13 +5,41 @@ const express = require('express');
 const app = new express();
 
 const path = require('path');
+
+// Make a connection with View
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Index Route
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+// Route
+app.get('/', (req, res) => res.render('index', { accounts: accounts }));
+
+app.get('/savings', (req, res) =>
+  res.render('account', { account: accounts.savings })
+);
+app.get('/checking', (req, res) =>
+  res.render('account', { account: accounts.checking })
+);
+app.get('/credit', (req, res) =>
+  res.render('account', { account: accounts.credit })
+);
+
+app.get()
+
+// Read AccountData and UserData
+const accountData = fs.readFileSync(
+  path.join(__dirname, 'json', 'accounts.json'),
+  'utf8'
+);
+const accounts = JSON.parse(accountData);
+console.log(accountData);
+const userData = fs.readFileSync(
+  path.join(__dirname, 'json', 'users.json'),
+  'utf8'
+);
+const users = JSON.parse(userData);
+console.log(users);
 
 // Listen to PORT = 3000
-app.listen(3000, () => console.log('PS Project Running on port 3000'))
+app.listen(3000, () => console.log('PS Project Running on port 3000'));
